964628379ddb1abb57c75fdbe40a3b71ea14dde9
annuk <annuk@ANAY-PC>
2016-06-19 11:58:28 -0700

    Add project files.

---
+++ SignalRapp/MyHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR.Hosting;
+using System.Web.UI.WebControls;
+
+namespace SignalRapp
+{   [HubName("myhub")]
+    public class MyHub : Hub
+    {
+        public void Send(string name,string message)
+        {
+            Clients.All.broadcastMessage(name,message);
+        }
+    }
+
+    public class FileUpload: WebControl
+    {
+        protected void Page_upload(object sender,EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SignalRapp/scripts/myscript.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+
+   
+    while (true) {
+        var name = prompt("what's your name?");
+        if (name != "" && name!=null) {
+           
+            $("#username").text(name);
+            $("#message").focus();
+            var chat = $.connection.myhub;
+            chat.client.broadcastMessage = function (name, message) {
+
+                console.log(message);
+                $("#heading").append('<tr><td>' + name + " says " + message + '</td></tr>')
+            };
+
+            $.connection.hub.start().done(function () {
+                $("#sendbutton").click(function () {
+                    if ($("#message").val() == '') {
+                        alert("enter a message");
+                    }
+                    else {
+                        chat.server.send(name, $("#message").val());
+                        console.log($("#message").val());
+                        $("#message").val("").focus();
+                    }
+                    
+
+                });
+            });
+            break;
+        }
+        
+        
+    }
+     
+    
+});
